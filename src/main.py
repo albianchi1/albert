d66decb4109e7b9746ed9e55932a26f3fd7c1f67
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2023-02-17 12:29:42 +0100

    ho modificato il codice main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
-def play(game_file: str) -> int:
-    #codice di avvio
-  pass
+import numpy as np
+class snake(stringa) -> int:
+  def __init__(mossedagioco, snake  scia):
+      
+      
+      n=len(mossedagioco);
+      
+      while i<n:
+          if snake ha più di un valore allora seguo
+              "ha più "
+              "alleora fai colors" 
+              "altrimenti nul"
+          end
+          
+      trova le differenze tra un numpy e il corrispondente vettore   Uù 
+  
+    
+  
+pass
