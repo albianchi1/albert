9e755f1e1c6160c8c0a18fc5ad4936d38117c727
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2023-02-22 10:56:19 +0100

    ho aggiornato il codice main, dopo una prima bozza di ragionamento, ho creato una funzione init per costruire il codice

--- src/main.py
+++ src/main.py
@@ -1,31 +1,52 @@
+#importo numpy pandas e matplotlib
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+"decido di creare la funzione che accetta una stringa in cui contiene
+"il file json con il campo da gioco
 
 def play(game_file: str) -> int:
     
-    n=1; #lunghezza iniziale del serpente
+    #n=1; #lunghezza iniziale del serpente
     
-    def ostacoli(self,blocks):
-        self.blocks
-        b.np=[0 2,
-              1 1,
-              3 1];
+    "ultimo ragionamento sull'impostazione della funzione"
+    
+    #creo una funzione init
+    
+    def init__(self,campo,ostacoli,cibo,vuoto,serpente,scia):
+        self.campo
+        self.ostacoli
+        self.cibo
+        self.vuoto
+        
+        
+        
+        
+        
+        
+        
+   "ragionamento iniziale sull'impostazione della funzione"     
+    
+    #creo la funzione ostacoli
+    #def ostacoli(self,blocks):
+        #self.blocks
+        #b.np=([[0,2],[1,1],[3,1]])
+  
         #se il serpente non incontra un ostacolo allora il gioco prosegue
-        while
+        #while
     
-    class mossedagioco(moves,start):
+    #class mossedagioco(moves,start):
         
     
-    class scia(mossedagioco, snake, scia)
+    #class scia(mossedagioco, snake, scia)
     
-    if #mossa è buona 
+    #if #mossa è buona 
         #testa serpente va avanti
-        if #incontra cibo
-            "n=n+1;"
-    else:
-        return n
+        #if #incontra cibo
+            #"n=n+1;"
+    #else:
+        #return n
         
   pass
 
