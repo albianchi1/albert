878db545073811f325b7cf6cff580763a3d7a9fd
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2023-02-23 11:07:27 +0100

    ho creato la classe campo

--- src/main.py
+++ src/main.py
@@ -3,52 +3,62 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-"decido di creare la funzione che accetta una stringa in cui contiene
-"il file json con il campo da gioco
+"""decido di creare la funzione che accetta una stringa in cui contiene
+il file json con il campo da gioco"""
 
 def play(game_file: str) -> int:
-    
+   
     #n=1; #lunghezza iniziale del serpente
     
-    "ultimo ragionamento sull'impostazione della funzione"
     
-    #creo una funzione init
     
-    def init__(self,campo,ostacoli,cibo,vuoto,serpente,scia):
-        self.campo
-        self.ostacoli
-        self.cibo
-        self.vuoto
+    class Campo:
         
-        
-        
-        
-        
-        
-        
-   "ragionamento iniziale sull'impostazione della funzione"     
+           "ultimo ragionamento sull'impostazione della funzione"
+            #creo una funzione init
     
-    #creo la funzione ostacoli
-    #def ostacoli(self,blocks):
-        #self.blocks
-        #b.np=([[0,2],[1,1],[3,1]])
+            def init__(self,campo,ostacoli,cibo,vuoto,serpente,scia):
+                self.campo
+                self.ostacoli
+                self.cibo
+                self.vuoto
+        
+                #campo=np.zeros((4,6)) ho creato la matrice campo
+        
+        
+        
+        
+        
+        
+        
+            "ragionamento iniziale sull'impostazione della funzione"     
+    
+            #creo la funzione ostacoli
+            
+            #def ostacoli(self,blocks):
+                #self.blocks
+                
+                #b.np=([[0,2],[1,1],[3,1]])
   
-        #se il serpente non incontra un ostacolo allora il gioco prosegue
-        #while
+                #se il serpente non incontra un ostacolo allora il gioco prosegue
+                
+                #while
     
-    #class mossedagioco(moves,start):
+            #class mossedagioco(moves,start):
         
     
-    #class scia(mossedagioco, snake, scia)
+            #class scia(mossedagioco, snake, scia)
     
-    #if #mossa è buona 
-        #testa serpente va avanti
-        #if #incontra cibo
-            #"n=n+1;"
-    #else:
-        #return n
+                #if #mossa è buona 
+                    #testa serpente va avanti
+                    
+                    #if #incontra cibo
+                        #"n=n+1;"
+                    
+                    #else:
+                        #return n
         
-  pass
+ pass
 
 
 #come collego il file json al codice?
