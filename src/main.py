9a5171d0de3e31e30214129163b5836f3fc1221a
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2023-05-20 12:30:22 +0200

    ho concluso la parte di codice che mi converte un file JSON in una lista di liste

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 """converto un file PNG in un matrice di tuple RGB"""
-import numpy as np
 from PIL import Image
 
 def my_load_image(filename: str) -> np.ndarray:
@@ -44,66 +43,11 @@ def my_load_json(filename: str) -> np.ndarray:
          
   pass
 
-my_load_json(data/field_02.json)
+return(field)
 
+"""fine conversione di un file JSON in una lista di liste"""
 
-
-"""decido di creare la funzione che accetta una stringa in cui contiene
-il file json con il campo da gioco"""
 
 def play(game_file: str) -> int:
-   
-    #n=1; #lunghezza iniziale del serpente
-    
-    
-    
-    class Campo:
-        
-           "ultimo ragionamento sull'impostazione della funzione"
-            #creo una funzione init
-    
-            def init__(self,campo,ostacoli,cibo,vuoto,serpente,scia):
-                self.campo
-                self.ostacoli
-                self.cibo
-                self.vuoto
-        
-                #campo=np.zeros((4,6)) ho creato la matrice campo
-        
-        
-        
-        
-        
-        
-        
-            "ragionamento iniziale sull'impostazione della funzione"     
-    
-            #creo la funzione ostacoli
-            
-            #def ostacoli(self,blocks):
-                #self.blocks
-                
-                #b.np=([[0,2],[1,1],[3,1]])
-  
-                #se il serpente non incontra un ostacolo allora il gioco prosegue
-                
-                #while
-    
-            #class mossedagioco(moves,start):
-        
-    
-            #class scia(mossedagioco, snake, scia)
-    
-                #if #mossa è buona 
-                    #testa serpente va avanti
-                    
-                    #if #incontra cibo
-                        #"n=n+1;"
-                    
-                    #else:
-                        #return n
-        
- pass
 
 
-#come collego il file json al codice?
