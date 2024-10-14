530b0c9c85f200a53a1152ac915282bc1c8b5c84
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2024-10-14 09:21:17 +0200

    ho cominciato a riscrivere il codice main sul file main2.py riprendendo le cose giuste scritte nel codice main.py

---
+++ src/main2.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+"""converto un file PNG in una matrice"""
+
+from PIL import Image
+
+def my_load_image(filename: str) -> np.ndarray:
+    img = Image.open(filename)
+    return np.array(img)
+
+img=Image.open("data/field_01.png")
+print(np.array(img))
+
+
+""" fine conversione file PNG in una matrice """
+
+
+""" converto un file JSON in una matrice """
+
+import json
+
+def my_load_json(filename: str) -> np.ndarray:
+  
+  with open(filename) as initial_field:
+      json_data = json.load(initial_field)
+      print(json_data)
+      
+      rows = json_data['rows']
+      cols = json_data['cols']
+      food = json_data['food']
+      blocks = json_data['blocks']
+      
+      field = np.zeros((rows, cols, 3), dtype=np.uint8)
+      
+      
+      for [x,y] in food:
+          field[x][y]=(255,128,0)
+          
+      for [x,y] in blocks:
+          field[x][y]=(255,0,0)
+          
+  return field
+
+""" fine conversione file JSON in una matrice """
+
+
+#esempio di come viene se richiamo la funzione con un dei file json
+print(my_load_json("data/field_02.json"))
+
+
+
+
+
+
+
+
